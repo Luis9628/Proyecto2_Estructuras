479ab05c8fe2f45d154f3a4dab811a5244964187
Luis Aguilar Monge <luis.aguilar.monge@est.una.ac.cr>
2023-11-17 21:46:00 -0600

    Modificación del main
    Se crea un menú para la utilización del código, ya sea leyendo un fuente o escribiendo el código.

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import Parser
+import os
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def menu():
+    opcion = 0
+    while True:
+        print('''MENU ANALIZADOR SEMANTICO
+         \t1: Línea de código
+         \t2: Leer archivo de C++
+         \t3: Salir''')
+        try:
+            opcion = int(input("Opción: ").strip())
+        except ValueError:
+            print("Error: Por favor, ingrese un número válido.")
+            continue  # Reiniciar el bucle
 
+        if opcion == 1:
+            while True:
+                linea = input("Ingrese una línea de código C++:\n")
+                if linea.strip():
+                    Parser.call_Parse(linea)
+                    break  # Salir del bucle interno
+                else:
+                    print("\nIngrese código C++ válido")
+            input("\nPresione enter para continuar...")
+        elif opcion == 2:
+            while True:
+                path = input("Ingrese la dirección del archivo a analizarse:\n")
+                try:
+                    with open(path, 'r', encoding='utf-8') as file:
+                        data = file.read()
+                        Parser.call_Parse(data)
+                    break  # Salir del bucle interno
+                except UnicodeDecodeError:
+                    print("Error: Problema con la codificación del archivo.")
+            input("\nPresione enter para continuar...")
+        elif opcion == 3:
+            print("¡Gracias por usar este programa! ¡Viva la Liga!")
+            break  # Salir del bucle principal
+        else:
+            print("Opción no válida. Inténtalo de nuevo.")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    menu()
